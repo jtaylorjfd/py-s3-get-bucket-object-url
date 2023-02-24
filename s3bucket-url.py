e367d4383c310181a3baa6aeb1c91155c87e05b4
jtaylorjfd <125874501+jtaylorjfd@users.noreply.github.com>
2023-02-24 10:53:44 +0000

    Add files via upload

---
+++ s3bucket-url.py
@@ -0,0 +1,15 @@
+import boto3
+import re
+s3 = boto3.client("s3",
+                  region_name='us-east-1',
+                  aws_access_key_id='AWS_ACCESS_KEY',       
+                  aws_secret_access_key='AWS_SECRET')
+
+bucket_response = s3.list_buckets()
+all_objects = s3.list_objects(Bucket = 'BUCKET')                  
+
+for i in all_objects['Contents']:
+    #string manipulation
+    title = 'APP '+ re.sub(r'^.*?APP', '', i['Key'])[1:-4]
+    url ='https://BUCKET.s3.REGION_NAME.amazonaws.com/'+ i['Key'].replace(" ", "+")
+    print('{ title: "'+title+'",'+'url: "'+ url+'"},')
