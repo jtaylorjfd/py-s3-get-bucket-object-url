f2cf706e420c961d3c2f501db5d7082d9d085e24
JTaylor <jtaylor@Elliots-MacBook-Pro-2.local>
2023-02-28 10:29:53 +0000

    added environmental variable placeholders

--- s3bucket-url.py
+++ s3bucket-url.py
@@ -1,15 +1,22 @@
 import boto3
 import re
+
+#environment variables
+BUCKET = ''
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY_ID = ''
+REGION_NAME = '' 
+
 s3 = boto3.client("s3",
-                  region_name='REGION_NAME',
-                  aws_access_key_id='AWS_ACCESS_KEY',       
-                  aws_secret_access_key='AWS_SECRET')
+                  region_name=REGION_NAME,
+                  aws_access_key_id=AWS_ACCESS_KEY_ID,     
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY_ID)
 
 bucket_response = s3.list_buckets()
-all_objects = s3.list_objects(Bucket = 'BUCKET')                  
+all_objects = s3.list_objects(Bucket = BUCKET)                  
 
 for i in all_objects['Contents']:
     #string manipulation
-    title = 'APP '+ re.sub(r'^.*?APP', '', i['Key'])[1:-4] 
+    title = 'APP '+ re.sub(r'^.*?APP', '', i['Key'])[1:-4] # This section is optional. Edit title to fit project
     url ='https://'+BUCKET+'.s3.'+REGION_NAME+'.amazonaws.com/'+ i['Key'].replace(" ", "+")
     print('{ title: "'+title+'",'+'url: "'+ url+'"},')
